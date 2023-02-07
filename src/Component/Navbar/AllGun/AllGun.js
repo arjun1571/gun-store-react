4250e87a4b3bfda7ee26b6a356bea5a9b19f4bbb
Arjun Das <arjundasback515@gmail.com>
2023-02-07 16:55:55 +0530

    props data pass and cart design

--- src/Component/Navbar/AllGun/AllGun.js
+++ src/Component/Navbar/AllGun/AllGun.js
@@ -14,12 +14,14 @@ const AllGun = () => {
 
     return (
         <div>
+            <h1 className='text-center mt-5 font-bold text-3xl'>Wellcome to Gun Store bangladesh</h1>
+            <p className='text-center mt-2'>MH Arms co is a Bangladesh government authorized Fire arms,ammunitions and accessories dealer..We import pistol, revolver, short gun, rifle, air gun</p>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10'>
             {
-                gun.map(gun => <SingleGun gun={gun}></SingleGun>)
+                gun.map(gun => <SingleGun gun={gun} key={gun.id}></SingleGun>)
             }
-            {/* <h1>this is all gun</h1>
-            <h1>{gun.length}</h1>
-            <SingleGun></SingleGun> */}
+            </div>
+            
         </div>
     );
 };
--- src/Component/Navbar/SingleGun/SingleGun.js
+++ src/Component/Navbar/SingleGun/SingleGun.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import "./SingleGun.css"
 
 const SingleGun = (props) => {
-    const {name,img}=props.gun
+    console.log(props);
+    const {name,img,price,category}=props.gun
     return (
-        <div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={img} alt="Shoes" /></figure>
-                <div className="card-body">
+        <div className='my-10 mx-auto' >
+            <div className="card w-96 bg-base-100 shadow-xl ">
+            <figure><img className='img' src={img} alt="Shoes" /></figure>
+                <div className="card-body ">
                     <h2 className="card-title">
                     {name}
                     <div className="badge badge-secondary">NEW</div>
                     </h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <p>Price:${price}</p>
+                    <p>Category: {category}</p>
                     <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Add TO Cart</div> 
-                    <div className="badge badge-outline">Delete</div>
+                    <div className="btn btn-accent">Add TO Cart</div> 
+                    <div className="btn btn-outline btn-error">Delete</div>
                     </div>
                 </div>
             </div>
